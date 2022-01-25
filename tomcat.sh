3e89d71bebbeef56b9d087034f8e7ed22c37788c
uss-technologies <85061553+uss-technologies@users.noreply.github.com>
2022-01-25 13:50:53 +0530

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,12 @@
+sudo yum install java-1.8.0-openjdk-devel -y
+cd /opt
+sudo yum install wget -y
+sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.58/bin/apache-tomcat-9.0.58.tar.gz
+sudo tar -xvf apache-tomcat-9.0.58.tar.gz
+sudo rm apache-tomcat-9.0.58.tar.gz
+sudo mv apache-tomcat-9.0.58 tomcat9
+sudo chmod 777 -R tomcat9
+sudo chown ec2-user:ec2-user -R tomcat9
+cd /opt/tomcat9
+./bin/startup.sh
+#./bin/shutdown.sh
