6b0d8a33f9ea65dfc0be53a9d555d77793dded8f
uss-technologies <85061553+uss-technologies@users.noreply.github.com>
2022-01-25 13:56:20 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,7 +1,7 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 cd /opt
 sudo yum install wget -y
-sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.58/bin/apache-tomcat-9.0.58.tar.gz
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.58/bin/apache-tomcat-9.0.58.tar.gz
 sudo tar -xvf apache-tomcat-9.0.58.tar.gz
 sudo rm apache-tomcat-9.0.58.tar.gz
 sudo mv apache-tomcat-9.0.58 tomcat9
